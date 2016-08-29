e27dc8ecea92ebc65c53763e6670d1699bb2e5c8
Nicolas Frankel <nicolas@frankel.ch>
2016-08-29 22:37:25 +0200

    Demo further check on nested model

--- src/test/java/org/springframework/samples/petclinic/integration/VetIT.java
+++ src/test/java/org/springframework/samples/petclinic/integration/VetIT.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,6 +26,7 @@ public class VetIT {
         mockMvc.perform(get("/vets.html"))
             .andExpect(status().isOk())
             .andExpect(model().attributeExists("vets"))
+            .andExpect(model().attribute("vets", hasProperty("vetList", hasSize(6))))
             .andExpect(view().name("vets/vetList"));
     }
 }
