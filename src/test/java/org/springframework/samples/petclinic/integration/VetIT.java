dea8c1c5a7a90a96db112f6923352db5f4b704f2
Nicolas Frankel <nicolas@frankel.ch>
2016-08-29 22:35:31 +0200

    Demo checking for existing model attribute

---
+++ src/test/java/org/springframework/samples/petclinic/integration/VetIT.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class VetIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldFill6VetsInModelAndReturnVetListViewWheBrowsingAllVets() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("vets"))
+            .andExpect(view().name("vets/vetList"));
+    }
+}
