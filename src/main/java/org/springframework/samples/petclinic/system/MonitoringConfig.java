b06aba4de9b0c7a253b8a61b5e7d7fdc9f9a03f3
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-25 16:29:55 +0200

    Add Graphite exporter

--- src/main/java/org/springframework/samples/petclinic/system/MonitoringConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/MonitoringConfig.java
@@ -1,5 +1,8 @@
 package org.springframework.samples.petclinic.system;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
 import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
@@ -14,5 +17,13 @@ public class MonitoringConfig {
     @ExportMetricWriter
     public MetricWriter jmxMetricWriter(MBeanExporter mBeanExporter) {
         return new JmxMetricWriter(mBeanExporter);
+    }
+
+    @Bean
+    GraphiteReporter graphiteExporter(MetricRegistry metricRegistry) {
+        Graphite graphite = new Graphite("localhost", 22003);
+        GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).prefixedWith("boot").build(graphite);
+        reporter.start(500, TimeUnit.MILLISECONDS);
+        return reporter;
     }
 }
