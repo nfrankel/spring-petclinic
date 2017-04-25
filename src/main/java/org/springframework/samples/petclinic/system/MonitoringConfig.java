88d76f49d8388461598a9b45971dc7f8ee4ab7ec
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-25 16:30:47 +0200

    Add custom metric

--- src/main/java/org/springframework/samples/petclinic/system/MonitoringConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/MonitoringConfig.java
@@ -4,11 +4,17 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
 import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MonitoringConfig {
@@ -25,5 +31,26 @@ public class MonitoringConfig {
         GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).prefixedWith("boot").build(graphite);
         reporter.start(500, TimeUnit.MILLISECONDS);
         return reporter;
+    }
+
+    @Bean
+    MyCustomMetric myCustomMetric(DropwizardMetricServices dropwizardMetricServices) {
+        return new MyCustomMetric(dropwizardMetricServices, new SecureRandom());
+    }
+
+    class MyCustomMetric {
+
+        private final DropwizardMetricServices dropwizardMetricServices;
+        private final Random random;
+
+        MyCustomMetric(DropwizardMetricServices dropwizardMetricServices, Random random) {
+            this.dropwizardMetricServices = dropwizardMetricServices;
+            this.random = random;
+        }
+
+        @Scheduled(fixedRate = 10000)
+        public void measureDemo() {
+            dropwizardMetricServices.submit("gauge.random", random.nextInt(100));
+        }
     }
 }
