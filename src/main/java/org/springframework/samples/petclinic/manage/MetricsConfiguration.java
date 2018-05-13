5961f2371296b4084cbb99c816c144591711b146
Nicolas Fränkel <nicolas@frankel.ch>
2018-05-13 14:16:40 +0200

    Add custom gauge

--- src/main/java/org/springframework/samples/petclinic/manage/MetricsConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/manage/MetricsConfiguration.java
@@ -1,9 +1,12 @@
 package org.springframework.samples.petclinic.manage;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
 
 @Configuration
 public class MetricsConfiguration {
@@ -16,5 +19,15 @@ public class MetricsConfiguration {
         counter.increment();
         counter.increment(5);
         return counter;
+    }
+
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+
+    @Bean
+    public Gauge dummyGauge(MeterRegistry registry, Random random) {
+        return Gauge.builder("dummy.gauge", random, r -> r.nextInt(10)).register(registry);
     }
 }
