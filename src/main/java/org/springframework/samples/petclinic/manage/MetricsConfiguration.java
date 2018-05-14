ae6a25f6f177187748af9cec113b882cb05aacfa
Nicolas Fränkel <nicolas@frankel.ch>
2018-05-14 20:57:46 +0200

    Add customizer

--- src/main/java/org/springframework/samples/petclinic/manage/MetricsConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/manage/MetricsConfiguration.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.manage;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,5 +30,10 @@ public class MetricsConfiguration {
     @Bean
     public Gauge dummyGauge(MeterRegistry registry, Random random) {
         return Gauge.builder("dummy.gauge", random, r -> r.nextInt(10)).register(registry);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer customizer() {
+        return registry -> registry.config().commonTags("petclinic");
     }
 }
